8a08595d0bfb712737368bf63a6e06e59efdf770
mattachu <github@matteaston.net>
2026-04-12 22:01:38 +0100

    Add chat 9 to links

--- scripts/generate_links.py
+++ scripts/generate_links.py
@@ -41,6 +41,7 @@ FILES = [
     "chats/chat6/chat6-index.md",
     "chats/chat7/chat7-index.md",
     "chats/chat8/chat8-index.md",
+    "chats/chat9/chat9-index.md",
     # Root
     "repo-structure.md",
     "README.md",
